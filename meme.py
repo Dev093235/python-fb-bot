bd5c9de4efcc80250d3200dccdcda94d1c163cf7
Dev093235 <dev431140@gmail.com>
2025-03-05 14:24:26 +0530

    Delete meme.py

--- meme.py
+++
@@ -1,8 +0,0 @@
-import random
-
-def send_meme():
-    memes = ["meme1.jpg", "meme2.jpg", "meme3.jpg"]
-    return random.choice(memes)
-
-if __name__ == "__main__":
-    print("Sending meme:", send_meme())
