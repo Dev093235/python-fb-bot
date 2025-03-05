9916435c2bc2331ac5d3987a1fcfd1a231260abd
Dev093235 <dev431140@gmail.com>
2025-03-05 15:12:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
+# Configurations for the bot
 
+FB_COOKIES_FILE = "cookies.json"  # Facebook login cookies
+FLIRTY_MESSAGES_FILE = "replies.py"  # Flirty message storage
+MEME_FOLDER = "memes/"  # Folder for meme images
+VOICE_REPLY_FOLDER = "voice_responses/"  # Folder for voice replies
+
+BOT_RUNNING_HOURS = 5  # Bot will run for 5 hours once started
